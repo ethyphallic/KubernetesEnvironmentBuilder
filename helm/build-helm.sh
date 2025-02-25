78cc956f1f90f692f2e24903f0accdb66a71d95a
Niatsuna <niatsuna@gmail.com>
2025-02-25 15:50:19 +0100

    Fix typo in name of installation script

--- helm/build-helm.sh
+++ helm/build-helm.sh
@@ -5,7 +5,7 @@ function check_enabled() {
 }
 
 ($DIR/kafka/install-strimzi.sh)
-(check_enabled .infra.chaosMeshEnabled && cd $DIR/infra/ && ./install-choas-mesh.sh)
+(check_enabled .infra.chaosMeshEnabled && cd $DIR/infra/ && ./install-chaos-mesh.sh)
 (check_enabled .kafka.uiEnabled && cd $DIR/kafka/ && ./install-kafka-ui.sh)
 (check_enabled .monitor.prometheusEnabled && cd $DIR/monitor/ && ./install-prometheus.sh)
 (check_enabled .monitor.energyMonitorEnabled && cd $DIR/monitor&& ./install-kepler.sh)
