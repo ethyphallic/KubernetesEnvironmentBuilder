f7397ac472e9447017ff4f2ff329704945b004e5
Niatsuna <niatsuna@gmail.com>
2025-03-04 15:12:46 +0100

    Update interact script for config scheme

--- interact.sh
+++ interact.sh
@@ -1,6 +1,5 @@
 ### KUBERNETES ###
 export DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export ID=$1
 
 alias k=kubectl
 function kn() {
@@ -16,64 +15,68 @@ function kn() {
 
 function build() {
     chmod +x build-k8s.sh
-    ./build-k8s.sh $ID
+    ./build-k8s.sh
     echo "Build Finished"
 }
 
 ### MONITOR ###
 function prometheus() {
-  if [ -z $ID ]; then
-      echo "No argument supplied during source operation"
-  else
-    export PROMETHEUS_NODE_PORT=$(kubectl get -n scalablemine-$ID-monitor -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-kube-prometheus-prometheus)
-    export PROMETHEUS_URL="minikube:${PROMETHEUS_NODE_PORT}"
-    echo "Prometheus url: $PROMETHEUS_URL"
+  NAMESPACE=$(cat $DIR/config.json | jq .monitor.namespace)
+  if [ -z $NAMESPACE ]; then
+    echo "No namespace set in config.json"
+    exit 1
   fi
+  export PROMETHEUS_NODE_PORT=$(kubectl get -n $NAMESPACE -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-kube-prometheus-prometheus)
+  export PROMETHEUS_URL="kube1-1:${PROMETHEUS_NODE_PORT}"
+  echo "Prometheus url: $PROMETHEUS_URL"
 }
 
 function grafana() {
-  if [ -z $ID ]; then
-      echo "No argument supplied during source operation"
-  else
-    $DIR/helm/monitor/build-grafana-dashboards.sh
-    export GRAFANA_NODE_PORT=$(kubectl get --namespace scalablemine-$ID-monitor -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-grafana)
-    export GRAFANA_URL=minikube:$GRAFANA_NODE_PORT
-    GRAFANA_PORT=$1
-    if [ -z $1 ]; then
-      GRAFANA_PORT=9091
-    fi
-    export GRAFANA_USERNAME=admin
-    export GRAFANA_PASSWORD=prom-operator
-    echo "Grafana url: ${GRAFANA_URL}"
-    echo "Username: ${GRAFANA_USERNAME}"
-    echo "Password: ${GRAFANA_PASSWORD}"
+  NAMESPACE=$(cat $DIR/config.json | jq .monitor.namespace)
+  if [ -z $NAMESPACE ]; then
+    echo "No namespace set in config.json"
+    exit 1
   fi
+  $DIR/helm/monitor/build-grafana-dashboards.sh
+  export GRAFANA_NODE_PORT=$(kubectl get --namespace $NAMESPACE -o jsonpath="{.spec.ports[0].nodePort}" services prometheus-grafana)
+  export GRAFANA_URL=kube1-1:$GRAFANA_NODE_PORT
+  GRAFANA_PORT=$1
+  if [ -z $1 ]; then
+    GRAFANA_PORT=9091
+  fi
+  export GRAFANA_USERNAME=admin
+  export GRAFANA_PASSWORD=prom-operator
+  echo "Grafana url: ${GRAFANA_URL}"
+  echo "Username: ${GRAFANA_USERNAME}"
+  echo "Password: ${GRAFANA_PASSWORD}"
 }
 
 function run() {
-    python3 $(pwd)/main.py $1
+    python3 $DIR/main.py $1
 }
 
 function stop_load() {
-    if [ -z $ID ]; then
-      echo "No argument supplied during source operation"
-    else
-      kubectl delete cm def-datasource -n scalablemine-$ID-load
-      kubectl delete cm def-load -n scalablemine-$ID-load
-      kubectl delete cm def-sink -n scalablemine-$ID-load
-      kubectl delete -f $DIR/build/load
+    NAMESPACE=$(cat $DIR/config.json | jq .load.namespace)
+    if [ -z $NAMESPACE ]; then
+      echo "No namespace set in config.json"
+      exit 1
     fi
+    kubectl delete cm def-datasource -n $NAMESPACE
+    kubectl delete cm def-load -n $NAMESPACE
+    kubectl delete cm def-sink -n $NAMESPACE
+    kubectl delete -f $DIR/build/load
 }
 
 function start_load() {
-    if [ -z $ID ]; then
-      echo "No argument supplied during source operation"
-    else
-      kubectl create cm def-datasource --from-file $DIR/build/load/datasource -n scalablemine-$ID-load
-      kubectl create cm def-load --from-file $DIR/build/load/load -n scalablemine-$ID-load
-      kubectl create cm def-sink --from-file $DIR/build/load/sink -n scalablemine-$ID-load
-      kubectl apply -f $DIR/build/load
+    NAMESPACE=$(cat $DIR/config.json | jq .load.namespace)
+    if [ -z $NAMESPACE ]; then
+      echo "No namespace set in config.json"
+      exit 1
     fi
+    kubectl create cm def-datasource --from-file $DIR/build/load/datasource -n $NAMESPACE
+    kubectl create cm def-load --from-file $DIR/build/load/load -n $NAMESPACE
+    kubectl create cm def-sink --from-file $DIR/build/load/sink -n $NAMESPACE
+    kubectl apply -f $DIR/build/load
 }
 
 function start_chaos() {
@@ -93,26 +96,33 @@ function kafka_destroy() {
 }
 
 function kafka_operator_restart() {
-  if [ -z $ID ]; then
-    echo "No argument supplied during source operation"
-  else
-    kubectl delete pod -l name=strimzi-cluster-operator -n scalablemine-$ID-kafka
+  NAMESPACE=$(cat $DIR/config.json | jq .kafka.namespace)
+  if [ -z $NAMESPACE ]; then
+    echo "No namespace set in config.json"
+    exit 1
   fi
+  kubectl delete pod -l name=strimzi-cluster-operator -n $NAMESPACE
 }
 
 function kafka() {
-    export BOOTSTRAP_URL=$(kubectl get -n kafka -o jsonpath="{.spec.ports[0].nodePort}" services power-kafka-external-bootstrap)
-    echo "Kafka bootstrap server url: minikube:$BOOTSTRAP_URL"
+  NAMESPACE=$(cat $DIR/config.json | jq .kafka.namespace)
+  if [ -z $NAMESPACE ]; then
+    echo "No namespace set in config.json"
+    exit 1
+  fi
+  export BOOTSTRAP_URL=$(kubectl get -n $NAMESPACE -o jsonpath="{.spec.ports[0].nodePort}" services power-kafka-external-bootstrap)
+  echo "Kafka bootstrap server url: kube1-1:$BOOTSTRAP_URL"
 }
 
 function kafka_ui() {
-  if [ -z $ID ]; then
-    echo "No argument supplied during source operation"
-  else
-    export KAFKA_UI_NODE_PORT=$(kubectl get -n scalablemine-$ID-kafka -o jsonpath="{.spec.ports[0].nodePort}" services kafka-ui)
-    export KAFKA_UI_URL=minikube:$KAFKA_UI_NODE_PORT
-    echo "Kafka-UI url: $KAFKA_UI_URL"
+  NAMESPACE=$(cat $DIR/config.json | jq .kafka.namespace)
+  if [ -z $NAMESPACE ]; then
+    echo "No namespace set in config.json"
+    exit 1
   fi
+  export KAFKA_UI_NODE_PORT=$(kubectl get -n $NAMESPACE -o jsonpath="{.spec.ports[0].nodePort}" services kafka-ui)
+  export KAFKA_UI_URL=kube1-1:$KAFKA_UI_NODE_PORT
+  echo "Kafka-UI url: $KAFKA_UI_URL"
 }
 
 function get_clusters() {
