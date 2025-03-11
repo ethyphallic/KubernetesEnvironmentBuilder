6e61197acef4e167d9fe7a37c26880ca74587225
Niatsuna <niatsuna@gmail.com>
2025-03-11 12:05:23 +0100

    Cleanup docker script

--- docker.sh
+++ docker.sh
@@ -1,29 +1,33 @@
 DIR=$(dirname "$(realpath "$0")")
 IMAGE_NAME="scalablemine-cluster-container"
+CONTAINER_NAME=$IMAGE_NAME
 
-# Check container status
-if docker container inspect "$IMAGE_NAME" &> /dev/null; then
-    # Container exists, check if it's running
-    if [ "$(docker container inspect -f '{{.State.Running}}' "$IMAGE_NAME")" == "true" ]; then
-        echo "Container $IMAGE_NAME is already running, attaching to it..."
-        docker attach "$IMAGE_NAME"
-    else
-        echo "Container $IMAGE_NAME exists but is not running, starting it..."
-        docker start -i "$IMAGE_NAME"
-    fi
+# Prepare Container
+# > Check current status
+if docker container inspect "$CONTAINER_NAME" &> /dev/null; then
+  # > Exists; Check running status
+  if [ "$(docker container inspect -f '{{.State.Running}}' "$CONTAINER_NAME")" == "true" ]; then
+    echo Container $CONTAINER_NAME is already running, attaching to it...
+    docker attach $CONTAINER_NAME
+  else
+    echo Container $CONTAINER_NAME exists but is not running, starting it...
+    docker start -i $CONTAINER_NAME
+  fi
 else
-    # Container doesn't exist, create and run it
-    # First check image
-    if (docker image inspect "$IMAGE_NAME" &> /dev/null); then
-      echo Found image with name $IMAGE_NAME
-    else
-      echo Building image $IMAGE_NAME
-      docker build -t $IMAGE_NAME .
-    fi
-    # Then create a new container with said image
-    echo "Creating and starting new container $IMAGE_NAME..."
-    MSYS_NO_PATHCONV=1 docker run -it \
+  # > Doesn't exis; Check image status
+  if docker image inspect "$IMAGE_NAME" &> /dev/null; then
+    echo Found image with name $IMAGE_NAME
+  else
+    echo Image $IMAGE_NAME not found, building ...
+    docker build -t $IMAGE_NAME .
+  fi
+
+  # > Build container
+  # Note: MYSY_NO_PATH_CONV is for execution on windows (git bash) and will be ignored on Linux / Mac
+  echo Creating new container $CONTAINER_NAME ...
+  MSYS_NO_PATHCONV=1 docker run -it \
       -v $HOME/.kube/config:/root/.kube/config \
       -v $DIR:/app \
-      --network=host --name=$IMAGE_NAME $IMAGE_NAME
+      --network=host --name=$CONTAINER_NAME \
+      $IMAGE_NAME
 fi
