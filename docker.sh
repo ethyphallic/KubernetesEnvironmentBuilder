75d3668c84a190e70ee4a8b0b0aa01e2fa39c633
Niatsuna <niatsuna@gmail.com>
2025-03-11 10:31:57 +0100

    Extend docker script to check image and container status

--- docker.sh
+++ docker.sh
@@ -1,12 +1,29 @@
 DIR=$(dirname "$(realpath "$0")")
-IMAGE_NAME="cluster-container"
-IMAGE_PRESENT=$(docker image inspect "$IMAGE_NAME" >/dev/null 2>&1)
+IMAGE_NAME="scalablemine-cluster-container"
 
-if ! $IMAGE_PRESENT; then
-  MSYS_NO_PATHCONV=1 docker build -t $IMAGE_NAME .
+# Check container status
+if docker container inspect "$IMAGE_NAME" &> /dev/null; then
+    # Container exists, check if it's running
+    if [ "$(docker container inspect -f '{{.State.Running}}' "$IMAGE_NAME")" == "true" ]; then
+        echo "Container $IMAGE_NAME is already running, attaching to it..."
+        docker attach "$IMAGE_NAME"
+    else
+        echo "Container $IMAGE_NAME exists but is not running, starting it..."
+        docker start -i "$IMAGE_NAME"
+    fi
+else
+    # Container doesn't exist, create and run it
+    # First check image
+    if (docker image inspect "$IMAGE_NAME" &> /dev/null); then
+      echo Found image with name $IMAGE_NAME
+    else
+      echo Building image $IMAGE_NAME
+      docker build -t $IMAGE_NAME .
+    fi
+    # Then create a new container with said image
+    echo "Creating and starting new container $IMAGE_NAME..."
+    MSYS_NO_PATHCONV=1 docker run -it \
+      -v $HOME/.kube/config:/root/.kube/config \
+      -v $DIR:/app \
+      --network=host --name=$IMAGE_NAME $IMAGE_NAME
 fi
-
-MSYS_NO_PATHCONV=1 docker run -it \
-  -v $HOME/.kube/config:/root/.kube/config \
-  -v $DIR:/app \
-  --network=host $IMAGE_NAME
