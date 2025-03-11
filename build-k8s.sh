39b6e64637a19a9285c656f6864d9dc0e95b471e
Niatsuna <niatsuna@gmail.com>
2025-03-11 16:49:20 +0100

    Fix jsonnet check

--- build-k8s.sh
+++ build-k8s.sh
@@ -11,8 +11,7 @@ mkdir -p "$DIR/build/load/sink"
 mkdir -p "$DIR/build/chaos"
 mkdir -p "$DIR/build/monitor"
 
-JSONNET_PRESENT=$(command -v jsonnet >/dev/null 2>&1)
-if $JSONNET_PRESENT; then
+if command -v jsonnet &>/dev/null; then
     # Use locally present jsonnet (Also used in the interactive docker container)
     echo "Using local jsonnet installation"
     jsonnet -m $DIR $DIR/k8s/main.jsonnet
