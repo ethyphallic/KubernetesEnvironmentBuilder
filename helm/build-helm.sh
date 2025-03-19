981d760f4efd830ed283e7a6c2a56f8d4232f2a6
Niatsuna <niatsuna@gmail.com>
2025-03-19 12:05:57 +0100

    Fix typo

--- helm/build-helm.sh
+++ helm/build-helm.sh
@@ -5,7 +5,7 @@ function check_enabled() {
   [ $(cat $DIR/../config.json | jq $1) == "true" ]
 }
 (check_enabled .kafka.strimziEnabled && cd $DIR/kafka/ && ./install-strimzi.sh)
-(check_enabled .infra.chaosMeshEnabled && cd $DIR/infra/ && ./install-choas-mesh.sh)
+(check_enabled .infra.chaosMeshEnabled && cd $DIR/infra/ && ./install-chaos-mesh.sh)
 (check_enabled .kafka.uiEnabled && cd $DIR/kafka/ && ./install-kafka-ui.sh)
 (check_enabled .monitor.prometheusEnabled && cd $DIR/monitor/ && ./install-prometheus.sh)
 (check_enabled .monitor.energyMonitorEnabled && cd $DIR/monitor&& ./install-kepler.sh)
